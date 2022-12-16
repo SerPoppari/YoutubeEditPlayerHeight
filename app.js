40aa131cb5ebf399a025d49bac629365804d78ed
SerPoppari <104081444+SerPoppari@users.noreply.github.com>
2022-12-16 16:14:03 +0200

    Background script
    Addon now notices when the tab URL changes through a background script. Because of that it only does things when it notices the "watch" in the URL. It also searches for the chat-div and changes the calculation based on if it finds it or not.
    
    Also removed the style.css-file, since it was not needed anymore.

--- app.js
+++ app.js
@@ -1,18 +1,63 @@
-waitForVideoElementToDisplay(function(){
-	waitForVideoHeightAndWidth(function(){
-		const videoDiv = document.getElementById("movie_player");
-		const vidHeight = videoDiv.firstChild.firstChild.videoHeight;
-		const vidWidth = videoDiv.firstChild.firstChild.videoWidth;
-		const root = document.documentElement;
-		root.style.setProperty('--widthMultiplier', vidWidth / vidHeight);
-		root.style.setProperty('--heightMultiplier', vidHeight / vidWidth);
-		window.scrollTo(0, 0);
-	});
-});
+let playerTheater = null;
+let video = null;
+let chat = false;
+let vidHeight = -1;
+let vidWidth = -1;
+let inWatch = false;
 
-function waitForVideoElementToDisplay(callback) {
+browser.runtime.onMessage.addListener((request) => {
+	if (window.location.href.toString().includes("://www.youtube.com/watch"))
+	{
+		inWatch = true;
+		waitForElementsToDisplay(function() {
+			playerTheater = document.getElementById("player-theater-container");
+			video = document.getElementById("movie_player").firstChild.firstChild;
+			vidHeight = video.videoHeight;
+			vidWidth = video.videoWidth;
+			playerTheater.style.setProperty('max-height', 'calc(100vh - 56px)', 'important');
+			playerTheater.style.setProperty('height', '100vh', 'important');
+			window.dispatchEvent(new Event('resize'));
+			window.scrollTo(0, 0);
+			waitForChatToDisplay(function() {
+				chat = true;
+				window.dispatchEvent(new Event('resize'));
+			});
+		});
+	}
+	else
+	{
+		inWatch = false;
+		playerTheater = null;
+		video = null;
+		chat = false;
+	}
+})
+
+window.onresize = function(event) {
+	if (video != null)
+	{
+		let trueWidth = window.innerWidth;
+		if (chat)
+		{
+			trueWidth -= 400;
+		}
+		if ((window.innerHeight - 56) * (video.videoWidth / video.videoHeight) <= trueWidth)
+		{
+			video.style.setProperty('max-height', (window.innerHeight - 56).toString() + 'px', 'important');
+			video.style.setProperty('max-width', ((window.innerHeight - 56) * (vidWidth / vidHeight)).toString() + 'px', 'important');
+		}
+		else
+		{
+			video.style.setProperty('max-height', (trueWidth * (vidHeight / vidWidth)).toString() + 'px', 'important');
+			video.style.setProperty('max-width', trueWidth.toString() + 'px', 'important');
+		}
+	}
+};
+
+function waitForElementsToDisplay(callback) {
   (function loopSearch() {
-    if (document.getElementById("movie_player") != null) {
+    if (document.getElementById("player-theater-container") != null
+	&& document.getElementById("movie_player") != null) {
       callback();
       return;
     }
@@ -22,9 +67,9 @@ function waitForVideoElementToDisplay(callback) {
   })();
 }
 
-function waitForVideoHeightAndWidth(callback) {
+function waitForChatToDisplay(callback) {
   (function loopSearch() {
-    if (document.getElementById("movie_player").firstChild.firstChild.videoHeight != 0) {
+    if (document.getElementById("chat") != null) {
       callback();
       return;
     }
---
+++ background.js
@@ -0,0 +1,17 @@
+console.log("background");
+
+browser.tabs.onUpdated.addListener(() => {
+	browser.tabs.query({
+    currentWindow: true,
+    active: true
+	}).then(sendMessageToTabs);
+});
+
+function sendMessageToTabs(tabs) {
+  for (let tab of tabs) {
+    browser.tabs.sendMessage(
+      tab.id,
+      {greeting: "Hi from background script"}
+    );
+  }
+}
