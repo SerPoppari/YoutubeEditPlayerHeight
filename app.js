94f342b9ce9dcd33b89def6fae4525130aeed5e1
SerPoppari <104081444+SerPoppari@users.noreply.github.com>
2023-02-27 18:47:34 +0200

    A lot of changes
    Changed from "Improve Youtube" to "Enhancer for Youtube" last update, since it broke the frontpage. Now edited addon to work better with new addon. Addon is much more simple, and more reliable.

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 let playerTheater = null;
 let videoParent = null;
-let video = null;
-let chat = false;
 let lastURL = "";
-let fullscreen = false;
+
 let mutationObserver = new MutationObserver(function(mutations) {
 	if (!fullscreen && videoParent.getAttribute('aria-label').includes("Fullscreen"))
 	{
-		fullscreen = true;
-		window.dispatchEvent(new Event('resize'));
+		playerTheater.style.setProperty('max-height', '100vh', 'important');
 	}
 	else if (fullscreen && !videoParent.getAttribute('aria-label').includes("Fullscreen"))
 	{
-		fullscreen = false;
-		window.dispatchEvent(new Event('resize'));
+		playerTheater.style.setProperty('max-height', 'calc(100vh - 56px)', 'important');
 	}
 });
 
@@ -23,80 +19,23 @@ browser.runtime.onMessage.addListener((request) => {
 		if (lastURL != window.location.href)
 		{
 			lastURL = window.location.href;
-			chat = false;
 			fullscreen = false;
 			waitForElementsToDisplay(function() {
 				playerTheater = document.getElementById("player-theater-container");
 				videoParent = document.getElementById("movie_player");
+				playerTheater.style.setProperty('max-height', 'calc(100vh - 56px)', 'important');
 				mutationObserver.observe(videoParent, {
 					attributes: true
 				});
-				video = videoParent.firstChild.firstChild;
-				playerTheater.style.setProperty('height', '100vh', 'important');
-				window.dispatchEvent(new Event('resize'));
-				waitForChatToDisplay(function() {
-					chat = true;
-					window.dispatchEvent(new Event('resize'));
-				});
-				window.scrollTo(0, 0);
 			});
 		}
 	}
-	else
-	{
-		playerTheater = null;
-		video = null;
-		chat = false;
-	}
 })
-
-window.onresize = function(event) {
-	if (video != null)
-	{
-		let trueWidth = window.innerWidth;
-		if (chat)
-		{
-			trueWidth -= 400;
-		}
-		let trueHeight = window.innerHeight;
-		if (fullscreen)
-		{
-			playerTheater.style.setProperty('max-height', '100vh', 'important');
-		}
-		else
-		{
-			playerTheater.style.setProperty('max-height', 'calc(100vh - 56px)', 'important');
-			trueHeight -= 56;
-		}
-		if (trueHeight * (video.videoWidth / video.videoHeight) <= trueWidth)
-		{
-			video.style.setProperty('max-height', trueHeight.toString() + 'px', 'important');
-			video.style.setProperty('max-width', (trueHeight * (video.videoWidth / video.videoHeight)).toString() + 'px', 'important');
-		}
-		else
-		{
-			video.style.setProperty('max-height', (trueWidth * (video.videoHeight / video.videoWidth)).toString() + 'px', 'important');
-			video.style.setProperty('max-width', trueWidth.toString() + 'px', 'important');
-		}
-	}
-};
 
 function waitForElementsToDisplay(callback) {
   (function loopSearch() {
     if (document.getElementById("player-theater-container") != null
 	&& document.getElementById("movie_player") != null) {
-      callback();
-      return;
-    }
-    else {
-      setTimeout(function() { loopSearch(); }, 1000);
-    }
-  })();
-}
-
-function waitForChatToDisplay(callback) {
-  (function loopSearch() {
-    if (document.getElementById("chat") != null) {
       callback();
       return;
     }
